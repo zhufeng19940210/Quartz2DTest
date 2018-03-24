58f4bf1f54f7967368a479232c3fd9a8a0dc7305
zhufeng <358244144@qq.com>
2018-03-23 18:39:53 -0700

    提交下

--- Quartz2DTest/Classes/Controller/DrawView.h
+++ Quartz2DTest/Classes/Controller/DrawView.h
@@ -1,13 +1,14 @@
-//
 //  DrawView.h
 //  Quartz2DTest
-//
 //  Created by bailing on 2018/3/23.
 //  Copyright © 2018年 zhufeng. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
-
 @interface DrawView : UIView
-
+@property (nonatomic,strong)UIColor *pathColor;
+@property (nonatomic,strong)NSInteger lineWidth;
+@property (nonatomic,strong)UIImage *image;
+//请屏幕
+-(void)clear;
+//撤销
+-(void)undo;
 @end
--- Quartz2DTest/Classes/Controller/DrawView.m
+++ Quartz2DTest/Classes/Controller/DrawView.m
@@ -5,9 +5,43 @@
 #import "DrawView.h"
 #import "DrawPath.h"
 @interface DrawView()
-@property (nonatomic,strong)
+@property (nonatomic,strong)DrawPath *path;
+@property (nonatomic,strong)NSMutableArray *pathArray;
 @end
 @implementation DrawView
+-(NSMutableArray *)pathArray{
+    if (!_pathArray) {
+        _pathArray = [NSMutableArray array];
+    }
+    return _pathArray;
+}
+-(void)setImage:(UIImage *)image{
+    _image = image;
+    [self.pathArray addObject:image];
+    [self setNeedsDisplay];
+}
+-(instancetype)initWithFrame:(CGRect)frame{
+    if (self = [super initWithFrame:frame]) {
+        [self setUp];
+    }
+    return self;
+}
+-(void)awakeFromNib{
+    [super awakeFromNib];
+    [self setUp];
+}
+-(void)setUp{
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(pan:)];
+    [self addGestureRecognizer:pan];
+}
+-(void)pan:(UIGestureRecognizer *)pan{
+    CGPoint purT = [pan locationInView:self];
+    if (pan.state == ) {
+        <#statements#>
+    }
+}
 
-
+-(void)drawRect:(CGRect)rect{
+    
+}
 @end
